40497f404f93f9fddac33615fd0d9f72189354db
PavloKPO <pavlokolykhanov@gmail.com>
2022-08-26 16:36:17 +0300

    6.2 Character

--- Assets/_Game/Scripts/PlayerMovement.cs
+++ Assets/_Game/Scripts/PlayerMovement.cs
@@ -3,25 +3,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speedPlayer;
-
-    private Animator _myAnimator;
-    private float _horMovement;
-    private float _verMovement;
-    void Start()
+    private Animator _animator;
+    
+    private void Start()
     {
-        _myAnimator = GetComponent<Animator>();
+        _animator = GetComponent<Animator>();
     }
 
     
-    void Update()
+    private void Update()
     {
-        _horMovement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(_horMovement, 0, 0) * _speedPlayer * Time.deltaTime;
-
-        _verMovement = Input.GetAxis("Vertical");
-        transform.position += new Vector3(0, 0, _verMovement) * _speedPlayer * Time.deltaTime;
-
-        _myAnimator.SetFloat("ForAndBack", _verMovement);
-        _myAnimator.SetFloat("RightAndLeft", _horMovement);
+       float _horMovement = Input.GetAxis("Horizontal");
+       float _verMovement = Input.GetAxis("Vertical");
+        transform.position += new Vector3(_horMovement, 0, _verMovement).normalized * _speedPlayer * Time.deltaTime;       
+        
+        _animator.SetFloat("ForAndBack", _verMovement);
+        _animator.SetFloat("RightAndLeft", _horMovement);
     }
 }
--- Assets/_Game/Scripts/СameraСontroller.cs
+++ Assets/_Game/Scripts/СameraСontroller.cs
@@ -4,14 +4,13 @@ public class СameraСontroller : MonoBehaviour
 {
     [SerializeField] private Transform _player;
     [SerializeField] private float _trackingSpeed;
-
-    private Vector3 _target;           
+              
        
-    void Update()
+    private void Update()
     {
+        Vector3 _target = new Vector3(_player.position.x, transform.position.y, transform.position.z);
+
         Vector3 currentPosition = Vector3.Lerp(transform.position, _target, _trackingSpeed *Time.deltaTime);
         transform.position = currentPosition;
-
-        _target = new Vector3(_player.position.x, transform.position.y, transform.position.z);
     }
 }
