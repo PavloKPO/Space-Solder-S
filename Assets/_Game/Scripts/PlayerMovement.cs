cb5abb5410d48e9bcda08b1d263c26ffb6b39833
PavloKPO <pavlokolykhanov@gmail.com>
2022-09-01 20:43:19 +0300

    6.3 Control and Shooting

--- Assets/_Game/Scripts/PlayerMovement.cs
+++ Assets/_Game/Scripts/PlayerMovement.cs
@@ -2,18 +2,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _speedPlayer;    
+    [SerializeField] private float _speedMove;    
     private Animator _animator;
+    private CharacterController _characterController;
+    private Vector3 _moveVector;
     
+
+
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _characterController = GetComponent<CharacterController>();
     }
 
     
     private void Update()
     {
-        MovementPlayer();
+        //MovementPlayer();
+        CharacterMove();
     }
 
     private void MovementPlayer()
@@ -24,9 +30,29 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(horMovement, 0, verMovement);
         movement = Vector3.ClampMagnitude(movement, 1.0f);
 
-        transform.position += movement * _speedPlayer * Time.deltaTime;
+        transform.position += movement * _speedMove * Time.deltaTime;
 
         _animator.SetFloat("ForAndBack", verMovement);
         _animator.SetFloat("RightAndLeft", horMovement);
     }
+
+    private void CharacterMove()
+    {
+        _moveVector = Vector3.zero;
+        _moveVector.x = Input.GetAxis("Horizontal") * _speedMove;
+        _moveVector.z = Input.GetAxis("Vertical") * _speedMove;
+
+        if(Vector3.Angle(Vector3.forward, _moveVector) > 1f || Vector3.Angle(Vector3.forward, _moveVector) == 0)
+        {
+            Vector3 direct = Vector3.RotateTowards(transform.forward, _moveVector, _speedMove, 0.0f);
+            transform.rotation = Quaternion.LookRotation(direct);
+        }
+
+        _characterController.Move(_moveVector * Time.deltaTime);
+
+        _animator.SetFloat("ForAndBack", _moveVector.z);
+        _animator.SetFloat("RightAndLeft", _moveVector.x);
+    }
+
+
 }
--- Assets/_Game/Scripts/СameraСontroller.cs
+++ Assets/_Game/Scripts/СameraСontroller.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class СameraСontroller : MonoBehaviour
 {
     [SerializeField] private Transform _player;
-    [SerializeField] private float _trackingSpeed;
-              
+    [SerializeField] private float _trackingSpeed; 
+                 
        
     private void Update()
     {
-        Vector3 _target = new Vector3(_player.position.x, transform.position.y, transform.position.z);
+        Vector3 _target = new Vector3(_player.position.x, transform.position.y, _player.position.z - transform.position.z);
 
         Vector3 currentPosition = Vector3.Lerp(transform.position, _target, _trackingSpeed *Time.deltaTime);
         transform.position = currentPosition;
+      
     }
+
+
+    
+       
+    
 }
